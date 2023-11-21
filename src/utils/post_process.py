8ea04b95e06ad21c568ccafa758384111039622e
beatwad <beatwad@gmail.com>
2023-11-21 17:37:47 +0300

    Return postprocess to previous version

--- src/utils/post_process.py
+++ src/utils/post_process.py
@@ -22,14 +22,15 @@ def post_process_for_seg(
     records = []
     for series_id in unique_series_ids:
         series_idx = np.where(series_ids == series_id)[0]
-        this_series_sleeps = preds[series_idx][:, :, [0, 0]]
+        # this_series_sleeps = preds[series_idx][:, :, [0, 0]]
         this_series_preds = preds[series_idx][:, :, [1, 2]].reshape(-1, 2)
 
-        sleep_diffs = np.diff(this_series_sleeps[:, :, 0], prepend=1).flatten()
-        mask = (sleep_diffs > 0) & (sleep_diffs < 0.0001)
-        this_series_preds[mask, 0] += sleep_diffs[mask]
-        mask = (sleep_diffs < 0) & (sleep_diffs > -0.001)
-        this_series_preds[mask, 1] -= sleep_diffs[mask]
+        # sleep_diffs = np.diff(this_series_sleeps[:, :, 0], prepend=1).flatten()
+        # sleep_diffs = np.clip(sleep_diffs, -0.001, 0.0001)
+        # mask = sleep_diffs > 0  # & (sleep_diffs < 0.0001)
+        # this_series_preds[mask, 0] += sleep_diffs[mask]
+        # mask = sleep_diffs < 0  # & (sleep_diffs > -0.001)
+        # this_series_preds[mask, 1] -= sleep_diffs[mask]
 
         for i, event_name in enumerate(["onset", "wakeup"]):
             this_event_preds = this_series_preds[:, i]
@@ -59,8 +60,8 @@ def post_process_for_seg(
     sub_df = pl.DataFrame(records).sort(by=["series_id", "step"])
     row_ids = pl.Series(name="row_id", values=np.arange(len(sub_df)))
     sub_df = sub_df.with_columns(row_ids).select(["row_id", "series_id", "step", "event", "score"])
-    sub_df = sub_df.with_columns(
-        pl.when(pl.col("score") < 0).then(0).otherwise(pl.col("score")).alias("score"))
-    sub_df = sub_df.with_columns(
-        pl.when(pl.col("score") > 1).then(1).otherwise(pl.col("score")).alias("score"))
+    # sub_df = sub_df.with_columns(
+    #     pl.when(pl.col("score") < 0).then(0).otherwise(pl.col("score")).alias("score"))
+    # sub_df = sub_df.with_columns(
+    #     pl.when(pl.col("score") > 1).then(1).otherwise(pl.col("score")).alias("score"))
     return sub_df
